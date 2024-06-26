d8eb7480920321c3bba309e746d3e2e1a26f109c
Pedro Tomé <112412818+Ptome2000@users.noreply.github.com>
2024-06-26 01:16:14 +0100

    + Contacts

--- index.js
+++ index.js
@@ -2,30 +2,29 @@
   Have focus outline only for keyboard users 
  ---------------------------------------- */
 
- const handleFirstTab = (e) => {
-    if(e.key === 'Tab') {
-      document.body.classList.add('user-is-tabbing')
-  
-      window.removeEventListener('keydown', handleFirstTab)
-      window.addEventListener('mousedown', handleMouseDownOnce)
-    }
-  
+const handleFirstTab = (e) => {
+  if (e.key === "Tab") {
+    document.body.classList.add("user-is-tabbing");
+
+    window.removeEventListener("keydown", handleFirstTab);
+    window.addEventListener("mousedown", handleMouseDownOnce);
   }
-  
-  const handleMouseDownOnce = () => {
-    document.body.classList.remove('user-is-tabbing')
-  
-    window.removeEventListener('mousedown', handleMouseDownOnce)
-    window.addEventListener('keydown', handleFirstTab)
-  }
-  
-  window.addEventListener('keydown', handleFirstTab)
-  
-  // Get the button:
+};
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove("user-is-tabbing");
+
+  window.removeEventListener("mousedown", handleMouseDownOnce);
+  window.addEventListener("keydown", handleFirstTab);
+};
+
+window.addEventListener("keydown", handleFirstTab);
+
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -35,31 +34,27 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var popoverTrigger = document.getElementById('avatar');
-    var popover = new bootstrap.Popover(popoverTrigger, {
-      trigger: 'manual'
-    });
-
-    popover.show();
-
-    popoverTrigger.addEventListener('click', function (e) {
-      e.stopPropagation();
-      popover.toggle();
-    });
-
-    document.addEventListener('click', function (e) {
-      if (!popoverTrigger.contains(e.target)) {
-        popover.hide();
-      }
-
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var popoverTrigger = document.getElementById("avatar");
+  var popover = new bootstrap.Popover(popoverTrigger, {
+    trigger: "manual",
   });
 
-  
+  popover.show();
+
+  popoverTrigger.addEventListener("click", function (e) {
+    e.stopPropagation();
+    popover.toggle();
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!popoverTrigger.contains(e.target)) {
+      popover.hide();
+    }
+  });
+});
